38b6566427c2cfefb688f22af32d568b5279faf2
Mirukai <arthassvitaliy@gmail.com>
2021-08-11 18:50:04 +0300

    fix range

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 "use strict";
 class RangeValidator {
     constructor(from, to) {
-        if(isNaN(from) || typeof from !== "number") {
-            throw TypeError("from must be a number")
-        }
-        if(isNaN(to) || typeof to !== "number") {
-            throw TypeError("to must be a number")
-        }
-        if (from > to) {
-            throw RangeError("from must be less than to")
-        }
-        if (to < from) {
-            throw RangeError("to must be more than from")
-        }
-        this._from = from
-        this._to = to
+        this.from = from
+        this.to = to
     }
     get from() {
         return this._from
     }
     set from(value) {
+        if(isNaN(value) || typeof value !== "number") {
+            throw TypeError("from must be a number")
+        }
+        if (value > this.to) {
+            throw RangeError("from must be less than to")
+        }
         this._from = value
     }
     get to() {
         return this._to
     }
     set to(value) {
+        if(isNaN(value) || typeof value !== "number") {
+            throw TypeError("to must be a number")
+        }
+        if (value < this.from) {
+            throw RangeError("to must be more than from")
+        }
         this._to = value
     }
-    getterRange () {
+    get range() {
+        return [`${this.from}`, `${this.to}`]
+    }
+    get betterRange() {
         let range = []
         for (let i = this._from ; i <= this._to ; i++) {
             range.push(i)
@@ -36,15 +39,10 @@ class RangeValidator {
         return range
     }
     validate (number) {
-        let range = this.getterRange()
-        for (let i = 0 ; i <= range.length ; i++) {
-            if (number === range[i]) {
-                return number
-            }
-            if (number !== range[i] && range[i] === this[this._to] ) {
-                throw RangeError("number is not in range")
-            }
+        if (number > this.to || number < this.from) {
+            throw RangeError("number is not in range")
         }
+        return number
     }
 }
     
